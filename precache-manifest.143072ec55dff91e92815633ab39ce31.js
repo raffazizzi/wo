26e09a834166ec1bfad9d91fedfccf106e0b495f
raffazizzi <raffaeleviglianti@gmail.com>
2020-01-14 22:00:36 -0500

    publish

---
+++ precache-manifest.143072ec55dff91e92815633ab39ce31.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c5f68a432855457d94dd9a6e9474045",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21d7a811710d5fe2ba76",
+    "url": "/static/css/main.0371c471.chunk.css"
+  },
+  {
+    "revision": "fc0a3febdacecdcf98ec",
+    "url": "/static/js/2.48306ccd.chunk.js"
+  },
+  {
+    "revision": "5bf8142bf0fe86acc5cad58aadf60a1a",
+    "url": "/static/js/2.48306ccd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "21d7a811710d5fe2ba76",
+    "url": "/static/js/main.2ae0e033.chunk.js"
+  },
+  {
+    "revision": "7fbc3e4b6b5b679a800c",
+    "url": "/static/js/runtime-main.ad817afe.js"
+  },
+  {
+    "revision": "40fa0fb205b78ba93b2b67f3954be903",
+    "url": "/static/media/anchor.40fa0fb2.svg"
+  }
+]);
