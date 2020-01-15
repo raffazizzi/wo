657c695732e11b6655970a4dedfbf4abb65fb732
raffazizzi <raffaeleviglianti@gmail.com>
2020-01-14 21:52:19 -0500

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,88 @@
 import React from 'react';
-import logo from './logo.svg';
+import PropTypes from 'prop-types';
+import WoData from './WoData';
 import './App.css';
+import anchor from './anchor.svg';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Box from '@material-ui/core/Box';
+
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#141926'
+    },
+    text: {
+      primary: {
+        main: '#141926'
+      },     
+    },
+    secondary: {
+      main: '#01A9DB',
+    },
+  },
+});
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <Typography
+      component="div"
+      role="tabpanel"
+      hidden={value !== index}
+      id={`full-width-tabpanel-${index}`}
+      aria-labelledby={`full-width-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box p={3}>{children}</Box>}
+    </Typography>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
 
 function App() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="header"><a href="https://airtable.com/tbl3m3QRI8q9N0n3l/viwh9YqwZxENXZhQV">
+        <img className="anchor" src={anchor}/></a></div>
+      <Paper>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          indicatorColor="primary"
+          centered
         >
-          Learn React
-        </a>
-      </header>
+          <Tab label="Weight"/>
+          <Tab label="Reps"/>
+          <Tab label="Sets"/>
+        </Tabs>
+      </Paper>
+        <div>
+          <TabPanel value={value} index={0}>        
+            <WoData show="Weight" scale="140"/>      
+          </TabPanel>
+          <TabPanel value={value} index={1}>
+            <WoData show="Reps" scale="20"/>
+          </TabPanel>
+          <TabPanel value={value} index={2}>
+            <WoData show="Sets" scale="10"/>
+          </TabPanel>
+        </div>
     </div>
   );
 }
---
+++ src/WoData.js
@@ -0,0 +1,232 @@
+import React, {useRef, useEffect} from 'react'
+import * as A from 'airtable'
+import * as d3 from 'd3'
+
+function WoData(props) {
+  
+  const svgEl = useRef(null)
+
+  useEffect(() => {
+
+    const base = new A({apiKey: 'keyABekhLaYIHre0d'}).base('appQx1tmm4vNz1Zvh');
+    let allRecords = []
+    let allExercisesData = {}
+
+    const stat = props.show
+
+    base('Log').select({
+        view: "Grid view"
+    }).eachPage(function page(records, fetchNextPage) {    
+      allRecords = allRecords.concat(records)
+      fetchNextPage()
+
+    }, function done(err) {
+      const exPerDate = allRecords.reduce((acc, r, i) => {
+        if (Object.keys(acc).indexOf(r.get('Date')) === -1) {
+          acc[r.get('Date')] = i
+        }
+        return acc
+      }, {})
+      
+      if (err) { console.error(err); return; }
+
+      base('Exercises').select({
+        view: "All",
+        fields: ['Name', stat]
+      }).eachPage(function page(records, fetchNextPage) {    
+        // allRecords = allRecords.concat(records)
+        for (const r of records) {
+          allExercisesData[r.id] = {
+            name: r.get("Name"),
+            stat: r.get(stat)
+          }
+        }
+        fetchNextPage()
+
+      }, function done(err) {
+        if (err) { console.error(err); return; }
+
+        // Average allRecords
+
+        const byDate = allRecords.reduce((acc, r) => {
+          const d = r.get('Date')
+          if (acc.has(d)) {
+            acc.get(d).push(r)
+          } else {
+            acc.set(d, [r])
+          }
+          return acc
+        }, new Map())
+
+        byDate.forEach((r, d) => {
+          byDate.set(d, r.reduce((acc, r) => {
+            const ex = allExercisesData[r.get('Exercise')].name
+            if (acc.has(ex)) {
+              const wData = acc.get(ex)
+              wData.stats.push(r.get(stat))
+              wData.averageStat = wData.stats.reduce((a, b) => a + b, 0) / wData.stats.length
+            } else {
+              // Calculate averages
+              acc.set(ex, {
+                date: d,
+                exercise: r.get('Exercise'),
+                stats: [r.get(stat)],
+                averageStat: r.get(stat)
+              })
+            }
+            return acc
+          }, new Map()))
+        })
+
+        let flatData = []
+        for (const d of byDate.values()) {
+          flatData = flatData.concat(Array.from(d))
+        }
+
+        const svg = d3.select(svgEl.current)
+        const margin = 80;
+        const width = 1000 - 2 * margin;
+        const height = 600 - 2 * margin;
+
+        const legend = svg.append("g")
+        legend.append("circle")
+          .attr('class', 'goal')
+          .attr("cx", 60)
+          .attr("cy", 50)
+          .attr("r", 5)
+
+        legend
+          .append('text')
+          .attr('class', 'label')
+          .attr('x', 110)
+          .attr('y', 55)
+          .attr('text-anchor', 'middle')
+          .text("Goal met!")
+
+        legend.append("circle")
+          .attr('class', 'goal nah')
+          .attr("cx", 175)
+          .attr("cy", 50)
+          .attr("r", 5)
+
+        legend
+          .append('text')
+          .attr('class', 'label')
+          .attr('x', 240)
+          .attr('y', 55)
+          .attr('text-anchor', 'middle')
+          .text("Goal not met :(")
+
+        legend.append("circle")
+          .attr('class', 'goal woah')
+          .attr("cx", 325)
+          .attr("cy", 50)
+          .attr("r", 5)
+
+        legend
+          .append('text')
+          .attr('class', 'label')
+          .attr('x', 395)
+          .attr('y', 55)
+          .attr('text-anchor', 'middle')
+          .text("Goal exceeded!")
+
+        const chart = svg.append('g')
+          .attr('transform', `translate(${margin}, ${margin})`);
+          
+        const xScale = d3.scaleBand()
+          .range([0, width])
+          .domain(Array.from(byDate.keys()))
+          .padding(0.6)
+        
+        const yScale = d3.scaleLinear()
+          .range([height, 0])
+          .domain([0, props.scale]);
+
+        const makeYLines = () => d3.axisLeft()
+          .scale(yScale)
+
+        chart.append('g')
+          .attr('transform', `translate(0, ${height})`)
+          .call(d3.axisBottom(xScale));
+
+        chart.append('g')
+          .call(d3.axisLeft(yScale));
+
+        chart.append('g')
+          .attr('class', 'grid')
+          .call(makeYLines()
+            .tickSize(-width, 0, 0)
+            .tickFormat('')
+          )
+
+        const barGroups = chart.selectAll()
+          .data(flatData)
+          .enter()
+          .append('g')
+
+        barGroups
+          .append('rect')
+          .attr('style', 'fill:#141926; filter:drop-shadow(0px 1px 1px rgba(0, 0, 0, 1))')
+          .attr('class', (g) => {
+            return g[0].replace(/ /g, '_')
+          })
+          .attr('x', (g, i) => {
+            const date = g[1].date
+            console.log(exPerDate[date])
+            // return xScale(date) + 1*i
+            return xScale(date) + ((i-exPerDate[date])*12)
+          })
+          .attr('y', (g) => yScale(g[1].averageStat))
+          .attr('height', (g) => height - yScale(g[1].averageStat))
+          .attr('width', 10)
+
+        barGroups
+          .append('circle')
+          .attr('class', (g) => {
+            const w = allExercisesData[g[1].exercise].stat
+            const aw = g[1].averageStat
+            if (aw > w) {
+              return 'goal woah'
+            } else if (aw < w) {
+              return 'goal nah'
+            }
+            return 'goal'
+
+          })
+          .attr("cx", (g, i) => {
+            const date = g[1].date
+            return (xScale(date) + (i-exPerDate[date])*12) + 5
+          })
+          .attr("cy", (g) => yScale(allExercisesData[g[1].exercise].stat))
+          .attr("r", 5)
+        
+          svg
+            .append('text')
+            .attr('class', 'label')
+            .attr('x', -(height / 2) - margin)
+            .attr('y', margin / 2.4)
+            .attr('transform', 'rotate(-90)')
+            .attr('text-anchor', 'middle')
+            .text(stat)
+
+          svg.append('text')
+            .attr('class', 'label')
+            .attr('x', width / 2 + margin)
+            .attr('y', height + margin * 1.7)
+            .attr('text-anchor', 'middle')
+            .text('Training days')
+
+      })
+    })
+
+  })
+
+  return (
+    <div style={{width: '1000px', height: '600px', margin: 'auto', backgroundColor: '#01A9DB'}}>
+      <svg style={{width: '100%', height: '100%'}} ref={svgEl} />
+    </div>
+  )
+}
+
+export default WoData
